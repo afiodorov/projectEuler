b445efbee452b938f88f3586fa082bf046000df9
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2015-02-12 14:15:37 +0000

    tidy-up

--- 079.hs
+++ 079.hs
@@ -1,9 +1,13 @@
 import Prelude hiding (iterate)
-import Data.Char (intToDigit)
+import Data.Char (intToDigit, digitToInt)
+import System.Environment (getArgs)
+
+readLines :: FilePath -> IO [String]
+readLines = fmap lines . readFile
 
 placeEverywhere :: a -> [a] -> [[a]]
 placeEverywhere a [] = [[a]]
-placeEverywhere a (x:xs) = [a:x:xs] ++ map ((:) x) (placeEverywhere a xs)
+placeEverywhere a (x:xs) = (a:x:xs) : map ((:) x) (placeEverywhere a xs)
 
 getAllCandidates :: (Ord a) => [a] -> [a] -> [[a]]
 getAllCandidates attempt [] = [attempt]
@@ -15,15 +19,15 @@ getAllCandidates (x:y:xs) workingPass = do
     if successFullAttempt (x:y:xs) candidate then return candidate
     else
         let
-            (afterY, beforeY) = break ((==) y) (reverse candidate)
-            putBefore = map (flip (++) (y:(reverse afterY)))
+            (afterY, beforeY) = break (y ==) (reverse candidate)
+            putBefore = map (++ (y: reverse afterY))
                 $ placeEverywhere x (init (reverse beforeY))
         in
             filter (successFullAttempt (x:y:xs)) (putAfter candidate) ++ putBefore
         where
             putAfter candidate = if x `elem` candidate then
-                let (beforeX, afterX) = break ((==) x) candidate in
-                    map ((++) beforeX) $ placeEverywhere y afterX
+                let (beforeX, afterX) = break (x ==) candidate in
+                    map (beforeX ++) $ placeEverywhere y afterX
             else []
 
 
@@ -46,62 +50,18 @@ keepListsWithMinLength a = filter (\b -> length b == minL) a
     where
         minL = minLength a
         minLength :: [[a]] -> Int
-        minLength [a] = length a
+        minLength [] = undefined
+        minLength [b] = length b
         minLength (x:xs) = min (length x) (minLength xs)
 
 
 main :: IO()
-main = print $ map (map intToDigit) $ foldl (\pass -> keepListsWithMinLength . (iterate pass)) [head input] input
-
-input :: [[Int]]
-input = [
-    [3, 1, 9],
-    [6, 8, 0],
-    [1, 8, 0],
-    [6, 9, 0],
-    [1, 2, 9],
-    [6, 2, 0],
-    [7, 6, 2],
-    [6, 8, 9],
-    [7, 6, 2],
-    [3, 1, 8],
-    [3, 6, 8],
-    [7, 1, 0],
-    [7, 2, 0],
-    [7, 1, 0],
-    [6, 2, 9],
-    [1, 6, 8],
-    [1, 6, 0],
-    [6, 8, 9],
-    [7, 1, 6],
-    [7, 3, 1],
-    [7, 3, 6],
-    [7, 2, 9],
-    [3, 1, 6],
-    [7, 2, 9],
-    [7, 2, 9],
-    [7, 1, 0],
-    [7, 6, 9],
-    [2, 9, 0],
-    [7, 1, 9],
-    [6, 8, 0],
-    [3, 1, 8],
-    [3, 8, 9],
-    [1, 6, 2],
-    [2, 8, 9],
-    [1, 6, 2],
-    [7, 1, 8],
-    [7, 2, 9],
-    [3, 1, 9],
-    [7, 9, 0],
-    [6, 8, 0],
-    [8, 9, 0],
-    [3, 6, 2],
-    [3, 1, 9],
-    [7, 6, 0],
-    [3, 1, 6],
-    [7, 2, 9],
-    [3, 8, 0],
-    [3, 1, 9],
-    [7, 2, 8],
-    [7, 1, 6] ]
+main = do
+    (fileName:_) <- getArgs
+    content <- readLines fileName
+    let
+        input = map (map digitToInt) content
+        initGuess = [head input]
+        genShortestPasses candidatePasses = keepListsWithMinLength . iterate candidatePasses
+        in
+        mapM_ (putStrLn . map intToDigit) $ foldl genShortestPasses initGuess input
