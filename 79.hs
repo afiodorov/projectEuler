86bbe8716885d2c70e59f57e009441e156270187
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2015-02-13 15:31:07 +0000

    shorter subsequence implementation

--- 79.hs
+++ 79.hs
@@ -9,16 +9,16 @@ placeEverywhere :: a -> [a] -> [[a]]
 placeEverywhere a [] = [[a]]
 placeEverywhere a (x:xs) = (a:x:xs) : map ((:) x) (placeEverywhere a xs)
 
-listsWithXBeforeLastY :: (Ord a) => a -> a -> [a] -> [[a]]
+listsWithXBeforeLastY :: (Eq a) => a -> a -> [a] -> [[a]]
 listsWithXBeforeLastY x y = map reverse . listsWithYAfterFirstX y x . reverse
 
-listsWithYAfterFirstX :: (Ord a) => a -> a -> [a] -> [[a]]
+listsWithYAfterFirstX :: (Eq a) => a -> a -> [a] -> [[a]]
 listsWithYAfterFirstX x y list  = let
     (beforeX, afterX) = break (x ==) list in
     map ((beforeX ++ [x]) ++) $ placeEverywhere y (tail afterX)
 
 -- generates updated passwords from an attempt
-shortestPasses :: (Ord a) => [a] -> [a] -> [[a]]
+shortestPasses :: (Eq a) => [a] -> [a] -> [[a]]
 shortestPasses attempt [] = [attempt]
 shortestPasses [] password = [password]
 shortestPasses [x] password = if x `elem` password then [password]
@@ -35,18 +35,13 @@ shortestPasses attempt@(x:y:xs) password = do
             filter (isSuccessFullAttempt attempt) withXBeforeY ++ withYAfterX
 
 -- checks that login attempt is successfull for a password
-isSuccessFullAttempt :: (Ord a) => [a] -> [a] -> Bool
+isSuccessFullAttempt :: (Eq a) => [a] -> [a] -> Bool
 isSuccessFullAttempt = isSubsequence
 
-isSubsequence :: (Ord a) => [a] -> [a] -> Bool
-isSubsequence _ [] = False
+isSubsequence :: (Eq a) => [a] -> [a] -> Bool
 isSubsequence [] _ = True
-isSubsequence [x] xs = x `elem` xs
-isSubsequence (x:y:xs) sequen =
-    isSubsequence (y:xs) (tail' (dropWhile (/= x) sequen))
-    where
-        tail' [] = []
-        tail' ys = tail ys
+isSubsequence (_:_) [] = False
+isSubsequence (x:xs) (y:ys) = isSubsequence (if x == y then xs else x:xs) ys
 
 keepWithMinLength :: [[a]] -> [[a]]
 keepWithMinLength xs = filter ((==) (minimum (map length xs)) . length) xs
